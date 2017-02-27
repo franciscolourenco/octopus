2746983154af17cfc69567f7a07e9b79ae04945b
Francisco Lourenco <aristidesfl@gmail.com>
2017-02-27 15:12:01 +0100

    Initial Commit

---
+++ octopus/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  octopus
+//
+//  Created by user on 27/02/17.
+//  Copyright © 2017 Betafabric. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
