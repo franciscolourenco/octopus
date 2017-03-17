bee0491b8e706e21e48b3f3248870c2540475eeb
Pepe Becker <mail@pepebecker.com>
2017-03-17 18:47:07 +0100

    Added some menu items

--- Tentacle/AppDelegate.swift
+++ Tentacle/AppDelegate.swift
@@ -8,17 +8,54 @@
 
 import Cocoa
 
+var appIsEnabled = true
+
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
-    @IBOutlet weak var window: NSWindow!
     @IBOutlet weak var statusMenu: NSMenu!
+    @IBOutlet weak var statusLabelItem: NSMenuItem!
+    @IBOutlet weak var checkForUpdatesItem: NSMenuItem!
+    @IBOutlet weak var toggleItem: NSMenuItem!
+    @IBOutlet weak var openHelpItem: NSMenuItem!
+    @IBOutlet weak var quitItem: NSMenuItem!
+    @IBOutlet weak var helpWindow: NSWindow!
     
     let statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
     
     var tabMode : Modal? = nil
-
-    @IBAction func quitClicked(_ sender: NSMenuItem) {
+    
+    func checkForUpdates() {
+        let alert = NSAlert()
+        alert.messageText = "You're up-to-date!"
+        alert.informativeText = "Tentacle 1.0 is currently the newest version available."
+        alert.addButton(withTitle: "OK")
+        alert.buttons[0].isHighlighted = true
+        alert.runModal()
+    }
+    
+    func toggle() {
+        if appIsEnabled {
+            self.toggleItem.title = "Enable Tentacle"
+            appIsEnabled = false
+            self.statusLabelItem.title = "Status: Disabled"
+        } else {
+            self.toggleItem.title = "Disable Tentacle"
+            appIsEnabled = true
+            self.statusLabelItem.title = "Status: Enabled"
+        }
+    }
+    
+    func openHelp() {
+        let alert = NSAlert()
+        alert.messageText = "Go find help somewhere else!"
+        alert.informativeText = "I haven't figured out yet how to show/hide a help window."
+        alert.addButton(withTitle: "OK")
+        alert.buttons[0].isHighlighted = true
+        alert.runModal()
+    }
+    
+    func quit() {
         NSApplication.shared().terminate(self)
     }
     
@@ -29,6 +66,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         statusItem.menu = statusMenu
         
         
+        self.checkForUpdatesItem.target = self
+        self.checkForUpdatesItem.action = #selector(checkForUpdates)
+        
+        self.toggleItem.target = self
+        self.toggleItem.action = #selector(toggle)
+        
+        self.openHelpItem.target = self
+        self.openHelpItem.action = #selector(openHelp)
+        
+        self.quitItem.target = self
+        self.quitItem.action = #selector(quit)
+        
+        
         func acquirePrivileges() -> Bool {
             let trusted = kAXTrustedCheckOptionPrompt.takeUnretainedValue()
             let privOptions: NSDictionary = [trusted as NSString: true]
@@ -37,13 +87,13 @@ class AppDelegate: NSObject, NSApplicationDelegate {
                 let alert = NSAlert()
                 alert.messageText = "Enable Octopus"
                 alert.informativeText = "Once you have enabled Octopus in System Preferences, click OK."
-                alert.beginSheetModal(for: self.window, completionHandler: { response in
+//                alert.beginSheetModal(for: self.window, completionHandler: { response in
                     if AXIsProcessTrustedWithOptions(privOptions) {
                         print("already have priviledges")
                     } else {
                         NSApp.terminate(self)
                     }
-                })
+//                })
             }
             return accessEnabled
         }
@@ -58,12 +108,15 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // Insert code here to tear down your application
     }
 
-    func startTapping () {
+    func startTapping() {
+        print("startTapping()")
         class TabMode : Modal {
             override func entered () {
+                print("entered()")
                 Keyboard.keyDown(key: .command)
             }
             override func exited () {
+                print("exited()")
                 Keyboard.keyUp(key: .command)
             }
         }
